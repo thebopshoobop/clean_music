d6693165ae5e887e269e7e1f26014cfe6881fd6c
Will Timpson <will.timpson@gmail.com>
2017-04-12 17:24:38 -0400

    Parameters and docstring

--- clean_music.py
+++ clean_music.py
@@ -6,7 +6,18 @@ import click
 
 
 @click.command()
-def clean():
-    """Quick script to clean up music directories"""
-
-    click.echo(os.listdir('.'))
+@click.option(
+    '-d', '--directory', default='.',
+    help='Specify a directory, rather than using the current one'
+)
+@click.option(
+    '-r', '--remove', is_flag=True,
+    help='''Remove small directories rather than displaying info'''
+)
+@click.argument('size', default=2)
+def clean(directory, remove, size):
+    """Quick script to clean up music directories:
+    * Displays improperly formatted directories
+    * Displays directories that are too small to likely have any music (default
+    2MB). You may specify a different maximum size.
+    """
