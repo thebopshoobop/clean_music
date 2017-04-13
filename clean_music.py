42898611c17c2dadf1e388434a90d8d37d153295
Will Timpson <will.timpson@gmail.com>
2017-04-13 11:59:59 -0400

    Checking in a function, Echo iteratively, Return full single disc directory

--- clean_music.py
+++ clean_music.py
@@ -8,6 +8,63 @@ from collections import Counter
 import click
 
 
+def check_dir(directory, size):
+    """Check the given directory for formatting problems."""
+
+    all_discs = []
+    disc_tags = Counter()
+    results = {
+        'art': {'label': 'Missing arwork?', 'dirs': []},
+        'one': {'label': 'Ending with 1?', 'dirs': []},
+        'disk': {'label': 'Mispelling? -> disk:', 'dirs': []},
+        'format': {'label': 'Improper disc tag?', 'dirs': []},
+        'the': {'label': 'Ending with the?', 'dirs': []},
+        'extra': {'label': 'Multiple disc tags?', 'dirs': []},
+        'single': {'label': 'Unneeded disc tags?', 'dirs': []},
+        'empty': {'label': 'No music?', 'dirs': []},
+    }
+
+    for root, dirs, files in os.walk(directory):
+        # Only check leaf directories
+        if not dirs:
+            # Missing album artwork
+            if 'albumart.jpg' not in files:
+                results['art']['dirs'].append(root)
+            # Folders ending with ' 1'
+            if re.search(r' 1$', root):
+                if not re.search(r'vol\.|volume', root, re.I):
+                    results['one']['dirs'].append(root)
+            # Disk mispelling
+            if re.search(r'disk', root, re.I):
+                results['disk']['dirs'].append(root)
+            # Disc not capitalized or without a '(' before and a ' ' after
+            if (
+                    re.search(r'\(disc\ \d{1,2}\)', root)
+                    or re.search(r'[^\(]disc', root, re.I)
+                    or re.search(r'disc[^\ ]', root, re.I)
+            ):
+                results['format']['dirs'].append(root)
+            # Ending with the
+            if re.search(r'the$', root, re.I):
+                results['the']['dirs'].append(root)
+            # Multiple dis[ck]s
+            if len(re.findall(r'disc|disk', root, re.I)) > 1:
+                results['extra']['dirs'].append(root)
+            # Single disc albums with disc tags
+            if re.search(r'\(disc|disk\ \d{1,2}\)', root, re.I):
+                all_discs.append(root)
+                disc_tags[root[:-9]] += 1
+            # No music
+            dirsize = sum(op.getsize(op.join(root, name)) for name in files)
+            if dirsize < size * 1000000:
+                results['empty']['dirs'].append(root)
+
+    results['single']['dirs'] = [n for n in all_discs
+                                 if disc_tags[n[:-9]] == 1]
+
+    return results
+
+
 @click.command()
 @click.option(
     '-d', '--directory', default='.',
@@ -25,69 +82,8 @@ def clean(directory, remove, size):
     2MB). You may specify a different maximum size.
     """
 
-    no_art = []
-    one_ending = []
-    disk = []
-    formatted = []
-    the_ending = []
-    extras = []
-    numbers = Counter()
-    empties = []
+    results = check_dir(directory, size)
 
-    for root, dirs, files in os.walk(directory):
-        # Only check leaf folders
-        if not dirs:
-            # Missing album artwork
-            if 'albumart.jpg' not in files:
-                no_art.append(root)
-            # Folders ending with ' 1'
-            if re.search(r' 1$', root):
-                if not re.search(r'vol\.|volume', root, re.I):
-                    one_ending.append(root)
-            # Disk mispelling
-            if re.search(r'disk', root, re.I):
-                disk.append(root)
-            # Disc not capitalized or without a '(' before and a ' ' after
-            if (
-                    re.search(r'\(disc\ \d{1,2}\)', root)
-                    or re.search(r'[^\(]disc', root, re.I)
-                    or re.search(r'disc[^\ ]', root, re.I)
-            ):
-                formatted.append(root)
-            # Ending with the
-            if re.search(r'the$', root, re.I):
-                the_ending.append(root)
-            # Multiple dis[ck]s
-            if len(re.findall(r'disc|disk', root, re.I)) > 1:
-                extras.append(root)
-            # Single disc albums with disc tags
-            if re.search(r'\(disc|disk\ \d{1,2}\)', root, re.I):
-                numbers[root[:-9]] += 1
-            # No music
-            dirsize = sum(op.getsize(op.join(root, name)) for name in files)
-            if dirsize < size * 1000000:
-                empties.append(root)
-
-    click.secho('Missing arwork?', bg='blue', fg='white', bold=True)
-    click.echo('\n'.join(no_art))
-
-    click.secho('Ending with 1?', bg='blue', fg='white', bold=True)
-    click.echo('\n'.join(one_ending))
-
-    click.secho('Mispelling? -> disk:', bg='blue', fg='white', bold=True)
-    click.echo('\n'.join(disk))
-
-    click.secho('Improper disc tag?', bg='blue', fg='white', bold=True)
-    click.echo('\n'.join(formatted))
-
-    click.secho('Ending with the?', bg='blue', fg='white', bold=True)
-    click.echo('\n'.join(the_ending))
-
-    click.secho('Multiple disc tags?', bg='blue', fg='white', bold=True)
-    click.echo('\n'.join(extras))
-
-    click.secho('Unneeded disc tags?', bg='blue', fg='white', bold=True)
-    click.echo('\n'.join([n for n, i in numbers.items() if i == 1]))
-
-    click.secho('No music?', bg='blue', fg='white', bold=True)
-    click.echo('\n'.join(empties))
+    for _, result in results.items():
+        click.secho(result['label'], bg='blue', fg='white', bold=True)
+        click.echo('\n'.join(sorted(result['dirs'])))
